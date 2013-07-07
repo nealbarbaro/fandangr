549c72adfdd72b498556e0fba0a49c23b0620b58
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-07 16:07:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandangr::Application.config.secret_token = '41047fd8832c44727c7ee35d0e8877942be5a150596dd9be6e2b33000acb1880e85e82b6905460ae8720aae5be545c0b4bfbc3c63e88f090d5681df7faf7ff3e'
